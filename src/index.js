e225fa464a98187015d40492f4ddd70d80fd3990
Inna-Olkhovskaya <115504107+Inna-Olkhovskaya@users.noreply.github.com>
2022-12-23 01:15:45 +0200

    add main settings
    1. подключены шрифты
    2. разбит проект на части/секции+моб.меню и модалки = файлы html
    3. сформированы паршалы к файлам .scss
    4. созданы js файлы под моб.меню и модалки
    5. созданы файл базовых настроек  _base.scss и resetting -файл
    6. оптимизированы иконки SVG и сформированы в спрайт (в папке images)
    7. подключены все части проекта в index- файлы соответственно

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './mobile_menu_header';
+import './modal_productcard_header';
+import './modal_ingredients_products';
+import './modal_readmore_about';
+import './modal_ourlocation_contacts';
+import './modal_franch_contacts';
