14b3dbeac17a6065fb0eb90048a28846223385ec
Inna-Olkhovskaya <115504107+Inna-Olkhovskaya@users.noreply.github.com>
2022-12-24 15:38:50 +0200

    feature/add scripts_main
    Добавила 2 файла для скриптов секции "Отзывы клиентов", сделала подключение

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import './mobile_menu_header';
 import './modal_productcard_header';
 import './modal_ingredients_products';
 import './modal_readmore_about';
+import './slick.min_customer_reviews';
+import './script_customer_reviews';
 import './modal_ourlocation_contacts';
 import './modal_franch_contacts';
