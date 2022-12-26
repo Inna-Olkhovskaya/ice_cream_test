e5e753eb7333a4b030da8e3ef52b68dcbdc0b97a
Anastasiia Savinova Occhiminuti <anastesie@hotmail.com>
2022-12-26 23:56:38 +0100

    modal_franch_contacts
    Found one mistake in HTML, corrected, probably solution to the problem why the modal window doesn't appear

--- src/modal_franch_contacts.js
+++ src/modal_franch_contacts.js
@@ -14,42 +14,42 @@
 
 
 
-(() => {
-  const refs = {
-    openMenuBtn: document.querySelector('[data-franchise-modal-open]'),
-    closeMenuBtn: document.querySelector('[data-franchise-modal-close]'),
-    menu: document.querySelector('[data-franchise-modal]'),
-    body: document.querySelector('body'),
-    menuList: document.querySelector('.menu-list'),
-  };
-
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
-  refs.menuList.addEventListener('click', removeMenu);
-
-  function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no-scroll');
-  }
-
-  function removeMenu() {
-    refs.menu.classList.add('is-hidden');
-    refs.body.classList.remove('no-scroll');
-  }
-})();
-
-
 // (() => {
-//     const refs = {
-//       openModalBtn: document.querySelector("[data-modal]"),
-//       closeModalBtn: document.querySelector("[data-modal-close]"),
-//       modal: document.querySelector("[data-modal]"),
-//     };
+//   const refs = {
+//     openMenuBtn: document.querySelector('[data-franchise-modal-open]'),
+//     closeMenuBtn: document.querySelector('[data-franchise-modal-close]'),
+//     menu: document.querySelector('[data-franchise-modal]'),
+//     body: document.querySelector('body'),
+//     menuList: document.querySelector('.menu-list'),
+//   };
+
+//   refs.openMenuBtn.addEventListener('click', toggleMenu);
+//   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+//   refs.menuList.addEventListener('click', removeMenu);
+
+//   function toggleMenu() {
+//     refs.menu.classList.toggle('is-hidden');
+//     refs.body.classList.toggle('no-scroll');
+//   }
+
+//   function removeMenu() {
+//     refs.menu.classList.add('is-hidden');
+//     refs.body.classList.remove('no-scroll');
+//   }
+// })();
+
+
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[[data-franchise-modal-open]"),
+      closeModalBtn: document.querySelector("[data-franchise-modal-close]"),
+      modal: document.querySelector("[data-franchise-modal]"),
+    };
   
-//     refs.openModalBtn.addEventListener("click", toggleModal);
-//     refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
   
-//     function toggleModal() {
-//       refs.modal.classList.toggle("is-hidden");
-//     }
-//   })();
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
